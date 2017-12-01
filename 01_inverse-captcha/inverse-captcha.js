3ca3b1214298ad9dcd3e27524403d3e966a141d2
Mihael Safaric <mihael.safaric@gmail.com>
2017-12-02 00:14:19 +0100

    Solve inverse captcha

--- 01_inverse-captcha/inverse-captcha.js
+++ 01_inverse-captcha/inverse-captcha.js
@@ -1,5 +1,22 @@
 class InverseCaptcha {
-  solve(captha) {
+  solve(captcha) {
+    if (!captcha && captcha !== '') {
+      throw new Error('Invalid captcha input');
+    }
+    const circledCaptcha = `${captcha}${captcha[0]}`;
+
+    const captchaDigits = circledCaptcha.split('').map((digit) => parseInt(digit, 10));
     
+    return captchaDigits.reduce((sum, currentDigit, currentIndex) => {
+      if (!currentIndex) {
+        return sum;
+      }
+
+      if (currentDigit === captchaDigits[currentIndex - 1]) {
+        sum += currentDigit;
+      }
+
+      return sum;
+    }, 0)
   }
 }
