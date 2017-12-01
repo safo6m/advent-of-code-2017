04ade1e4681c27481a5905f5c9a4cb481d93d5d3
Mihael Safaric <mihael.safaric@gmail.com>
2017-12-02 00:45:51 +0100

    Solve halfway captcha

--- 01_inverse-captcha/inverse-captcha.js
+++ 01_inverse-captcha/inverse-captcha.js
@@ -1,11 +1,19 @@
 class InverseCaptcha {
-  solve(captcha) {
+  _checkInputValidity(captcha) {
     if (!captcha && captcha !== '') {
       throw new Error('Invalid captcha input');
     }
-    const circledCaptcha = `${captcha}${captcha[0]}`;
+  }
 
-    const captchaDigits = circledCaptcha.split('').map((digit) => parseInt(digit, 10));
+  _convertStringToDigits(value) {
+    return value.split('').map((digit) => parseInt(digit, 10));
+  }
+
+  solveSimpleCaptcha(captcha) {
+    this._checkInputValidity(captcha);
+
+    const circledCaptcha = `${captcha}${captcha[0]}`;
+    const captchaDigits = this._convertStringToDigits(circledCaptcha);
     
     return captchaDigits.reduce((sum, currentDigit, currentIndex) => {
       if (!currentIndex) {
@@ -13,6 +21,29 @@ class InverseCaptcha {
       }
 
       if (currentDigit === captchaDigits[currentIndex - 1]) {
+        sum += currentDigit;
+      }
+
+      return sum;
+    }, 0)
+  }
+
+  solveHalfwayCaptcha(captcha) {
+    this._checkInputValidity(captcha);
+
+    const halfwayIndex = captcha.length / 2;
+    
+    const firstHalfOfCaptcha = captcha.slice(0, halfwayIndex);
+
+    const circledCaptcha = `${captcha}${firstHalfOfCaptcha}`;
+    const captchaDigits = this._convertStringToDigits(circledCaptcha);
+
+    return captchaDigits.reduce((sum, currentDigit, currentIndex) => {
+      if (!currentIndex) {
+        return sum;
+      }
+
+      if (currentDigit === captchaDigits[currentIndex - halfwayIndex]) {
         sum += currentDigit;
       }
 
