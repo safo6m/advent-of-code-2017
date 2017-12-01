9540bc5e00e25a695e08071f4cac053e10587738
Mihael Safaric <mihael.safaric@gmail.com>
2017-12-01 23:36:28 +0100

    Init sample task

---
+++ 01_inverse-captcha/inverse-captcha.js
@@ -0,0 +1,5 @@
+class InverseCaptcha {
+  decode() {
+    console.log('test');
+  }
+}
---
+++ 01_inverse-captcha/inverse-captcha.spec.js
@@ -0,0 +1,11 @@
+describe('Inverse Captcha', function() {
+  let inverseCaptcha;
+
+  beforeEach(function() {
+    inverseCaptcha = new InverseCaptcha();
+  });
+
+  it('test', function() {
+    inverseCaptcha.decode();
+  });
+});
