434a013a2ddfd937663512800014f8f67a4abb02
Mihael Safaric <mihael.safaric@gmail.com>
2017-12-02 00:54:46 +0100

    Improve inverse captcha

--- 01_inverse-captcha/inverse-captcha.js
+++ 01_inverse-captcha/inverse-captcha.js
@@ -9,23 +9,27 @@ class InverseCaptcha {
     return value.split('').map((digit) => parseInt(digit, 10));
   }
 
+  _solveCaptcha(captchaDigits, step) {
+    return captchaDigits.reduce((sum, currentDigit, currentIndex) => {
+      if (!currentIndex) {
+        return sum;
+      }
+
+      if (currentDigit === captchaDigits[currentIndex - step]) {
+        sum += currentDigit;
+      }
+
+      return sum;
+    }, 0)
+  }
+
   solveSimpleCaptcha(captcha) {
     this._checkInputValidity(captcha);
 
     const circledCaptcha = `${captcha}${captcha[0]}`;
     const captchaDigits = this._convertStringToDigits(circledCaptcha);
     
-    return captchaDigits.reduce((sum, currentDigit, currentIndex) => {
-      if (!currentIndex) {
-        return sum;
-      }
-
-      if (currentDigit === captchaDigits[currentIndex - 1]) {
-        sum += currentDigit;
-      }
-
-      return sum;
-    }, 0)
+    return this._solveCaptcha(captchaDigits, 1);
   }
 
   solveHalfwayCaptcha(captcha) {
@@ -38,16 +42,6 @@ class InverseCaptcha {
     const circledCaptcha = `${captcha}${firstHalfOfCaptcha}`;
     const captchaDigits = this._convertStringToDigits(circledCaptcha);
 
-    return captchaDigits.reduce((sum, currentDigit, currentIndex) => {
-      if (!currentIndex) {
-        return sum;
-      }
-
-      if (currentDigit === captchaDigits[currentIndex - halfwayIndex]) {
-        sum += currentDigit;
-      }
-
-      return sum;
-    }, 0)
+    return this._solveCaptcha(captchaDigits, halfwayIndex);
   }
 }
