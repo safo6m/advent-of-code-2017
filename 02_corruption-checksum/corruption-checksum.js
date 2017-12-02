c6b2c5d9e0544b25bfa2f318d69d3f9a5b6ea5db
Mihael Safaric <mihael.safaric@gmail.com>
2017-12-02 14:56:19 +0100

    Implement part two of corruption checksum

--- 02_corruption-checksum/corruption-checksum.js
+++ 02_corruption-checksum/corruption-checksum.js
@@ -1,5 +1,20 @@
 class CorruptionChecksum {
   calculateChecksum(spreadsheet) {
-    return spreadsheet.map((row) => row.sort((x, y) => y - x)).map((row) => row[0] - row[row.length - 1]).reduce((sum, rowDifference) => sum + rowDifference, 0);
+    return spreadsheet.map((row) => row.sort((x, y) => y - x))
+                      .map((row) => row[0] - row[row.length - 1])
+                      .reduce((sum, rowDifference) => sum + rowDifference, 0);
+  }
+
+  sumResultOfWholeDivisions(spreadsheet) {
+    return spreadsheet.map((row) => row.sort((x, y) => y - x))
+                      .map((row) => {
+                        return row.map((dividend, index) => {
+                          return row.slice(index + 1)
+                                    .filter((divisor) => divisor)
+                                    .map((divisor) => dividend / divisor)
+                                    .filter((quotient) => Number.isInteger(quotient))
+                                    .reduce((sum, currentValue) => sum + currentValue, 0);
+                        }).reduce((sum, rowResult) => sum + rowResult, 0);
+                      }).reduce((sum, rowResult) => sum + rowResult, 0);
   }
 }
